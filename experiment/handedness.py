1e76d731a938c52bce788a3c5913b874d2910b0b
Csxeba <csxeba@gmail.com>
2018-01-03 21:57:34 +0100

    Merged

--- experiment/handedness.py
+++ experiment/handedness.py
@@ -2,7 +2,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 from EBH.utility.frame import DataWrapper
-from EBH.utility.operation import load_dataset
+from EBH.experiment.plottransform import load_data
 
 
 def as_angle(stream, against=(0., 1., 0.)):
@@ -26,11 +26,36 @@ def peak_mean_angles():
     pass
 
 
-def main():
+def xperiment_angleplot():
     dw = DataWrapper("box4_fel")
     plotangles(dw.get_data("left"), "left")
     plotangles(dw.get_data("right"), "right")
 
 
-if __name__ == '__main__':
-    main()
+def xperiment_meanwavelets():
+    X, gesture, name, orient, ID, pro, hand = load_data(False)
+
+    N = len(X)
+    X = X.reshape(N, 20, 4).transpose(0, 2, 1).reshape(N, 80)
+
+    left, right = X[hand == "left"], X[hand == "right"]
+
+    left_mean, right_mean = left.mean(axis=0), right.mean(axis=0)
+    fig, axes = plt.subplots(2, 2, sharex=True, sharey=True)
+
+    ((tax, mtx), (btx, bax)) = axes
+
+    tax.plot(left_mean)
+    tax.set_title("left")
+    mtx.plot(right_mean)
+    mtx.set_title("right")
+    btx.plot(left_mean - right_mean)
+    btx.plot([0. for _ in range(len(left_mean))], "r-")
+    btx.set_title("diff")
+    bax.plot(X.mean(axis=0))
+    bax.set_title("mean")
+
+    for ax in axes.flat:
+        ax.grid(True)
+
+    plt.show()
